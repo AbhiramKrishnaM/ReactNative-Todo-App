6501ba8d9937d45213c8b8b5593c46b86ebdfd21
abhiramkrishna.8921@gmail.com <abhiramKrishna.8921@gmail.com>
2021-11-07 00:17:40 -0700

    Todos listed using flatlist

--- App.js
+++ App.js
@@ -3,15 +3,19 @@ import React, {useState} from "react";
 import {StyleSheet, View} from "react-native";
 
 import RenderTodo from "./Components/RenderTodo";
-
+import RenderList from "./Components/RenderList";
 export default function App() {
   const [todos, setTodos] = useState([]);
   function todoHandler(newTodo) {
-    setTodos((previousTodo) => [...previousTodo, newTodo]);
+    setTodos((previousTodo) => [
+      ...previousTodo,
+      {value: newTodo, key: Math.random().toString()},
+    ]);
   }
   return (
     <View style={styles.container}>
       <RenderTodo todoHandler={todoHandler} />
+      <RenderList todoData={todos} />
       <StatusBar style="auto" />
     </View>
   );
--- Components/RenderList.js
+++ Components/RenderList.js
@@ -1,12 +1,40 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from "react-native";
 
-const RenderList = () => {
+const RenderList = (props) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Todos</Text>
+      <FlatList
+        data={props.todoData}
+        renderItem={(itemData) => (
+          <TouchableOpacity activeOpacity={0.5} style={styles.listContainer}>
+            <Text style={styles.listText}>{itemData.item.value}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {marginTop: 30, width: "80%"},
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "orange",
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  listContainer: {
+    padding: 15,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: "orange",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  listText: {
+    color: "orange",
+  },
+});
 export default RenderList;
